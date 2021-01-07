d4dc1c5ee7f1e4ffd787c8d2a20ec27b8a8fabc9
Iyngaran Iyathurai <dev@iyngaran.info>
2021-01-07 20:45:43 +0530

    Added password reset request

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ Route::post('/register', 'RegisterController@register')->name('register');
 Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
 //Route::post('/reset-password', 'ResetPasswordController')->name('reset.password');
+Route::post('forgot-password', 'AuthController@forgotPassword');
 
 Route::middleware('auth:sanctum')->group(
     function () {
--- src/Http/Controllers/Api/AuthController.php
+++ src/Http/Controllers/Api/AuthController.php
@@ -3,11 +3,16 @@
 namespace Iyngaran\LaravelUser\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
 use Iyngaran\ApiResponse\Http\Traits\ApiResponse;
 use Iyngaran\LaravelUser\Repositories\UserRepositoryInterface;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Iyngaran\LaravelUser\Http\Resources\AuthenticatedUser as UserResource;
 use Iyngaran\LaravelUser\Http\Requests\LoginRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -42,4 +47,37 @@ class AuthController extends Controller
             )
         );
     }
+
+    public function forgotPassword(Request $request)
+    {
+        $input = $request->all();
+        $rules = array(
+            'email' => "required|email",
+        );
+        $validator = Validator::make($input, $rules);
+        if ($validator->fails()) {
+            $arr = array("status" => 400, "message" => $validator->errors()->first(), "data" => array());
+        } else {
+            try {
+                $response = Password::sendResetLink(
+                    $request->only('email'),
+                    function (Message $message) {
+                        $message->subject($this->getEmailSubject());
+                    }
+                );
+                switch ($response) {
+                    case Password::RESET_LINK_SENT:
+                        return response()->json(["message" => trans($response)], Response::HTTP_OK);
+                    case Password::INVALID_USER:
+                        return response()->json(["message" => trans($response)], Response::HTTP_BAD_REQUEST);
+                }
+            } catch (\Swift_TransportException $ex) {
+                $arr = array("status" => 400, "message" => $ex->getMessage(), "data" => []);
+            } catch (Exception $ex) {
+                $arr = array("status" => 400, "message" => $ex->getMessage(), "data" => []);
+            }
+        }
+        return \Response::json($arr);
+    }
+
 }
