b6d38f2ae99471b9a7332afc39c2a04b54ebf63c
Iyngaran Iyathurai <dev@iyngaran.info>
2021-01-16 22:47:43 +0530

    Added password reset table

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ Route::post('/login', 'AuthController@login')->name('login');
 Route::post('/register', 'RegisterController@register')->name('register');
 Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
-Route::post('/reset-password', 'ResetPasswordController')->name('reset.password');
+Route::post('/reset-password', 'ResetPasswordController')->name('password.reset');
 Route::post('forgot-password', 'AuthController@forgotPassword');
 
 Route::middleware('auth:sanctum')->group(
